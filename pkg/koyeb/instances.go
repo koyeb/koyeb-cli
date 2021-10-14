fb76428754e2c9f1c1c1caa21965ef6872901df3
nicoche <nicolas@koyeb.com>
2021-10-14 17:26:54 +0200

    Add koyeb instances exec skeleton

---
+++ pkg/koyeb/instances.go
@@ -0,0 +1,36 @@
+package koyeb
+
+import (
+	"errors"
+	"github.com/spf13/cobra"
+)
+
+func NewInstanceCmd() *cobra.Command {
+	h := NewInstanceHandler()
+	instanceCmd := &cobra.Command{
+		Use:     "instances [action]",
+		Aliases: []string{"i", "instance"},
+		Short:   "Instances",
+	}
+
+	execInstanceCmd := &cobra.Command{
+		Use:   "exec [name] [cmd] [cmd...]",
+		Short: "Execute command in instance's context",
+		Args:  cobra.MinimumNArgs(2),
+		RunE:  h.Exec,
+	}
+	instanceCmd.AddCommand(execInstanceCmd)
+
+	return instanceCmd
+}
+
+func NewInstanceHandler() *InstanceHandler {
+	return &InstanceHandler{}
+}
+
+type InstanceHandler struct {
+}
+
+func (h *InstanceHandler) Exec(*cobra.Command, []string) error {
+	return errors.New("unimplemented")
+}
