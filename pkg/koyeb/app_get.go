d7347dcb4ccc3d1eab8bc90da087af099cbe56c8
Bastien Chatelard <chatel.bast@gmail.com>
2021-12-03 23:06:31 +0100

    Add app get with item renderer

---
+++ pkg/koyeb/app_get.go
@@ -0,0 +1,62 @@
+package koyeb
+
+import (
+	"context"
+
+	"github.com/koyeb/koyeb-api-client-go/api/v1/koyeb"
+	"github.com/koyeb/koyeb-cli/pkg/koyeb/renderer"
+	"github.com/spf13/cobra"
+)
+
+func (h *AppHandler) Get(cmd *cobra.Command, args []string) error {
+	client := getApiClient()
+	ctx := getAuth(context.Background())
+
+	res, _, err := client.AppsApi.GetApp(ctx, args[0]).Execute()
+	if err != nil {
+		fatalApiError(err)
+	}
+
+	full, _ := cmd.Flags().GetBool("full")
+	getAppsReply := NewGetAppReply(&res, full)
+
+	output, _ := cmd.Flags().GetString("output")
+	return renderer.ItemRenderer(output, getAppsReply)
+}
+
+type GetAppReply struct {
+	res  *koyeb.GetAppReply
+	full bool
+}
+
+func NewGetAppReply(res *koyeb.GetAppReply, full bool) *GetAppReply {
+	return &GetAppReply{
+		res:  res,
+		full: full,
+	}
+}
+
+func (a *GetAppReply) MarshalBinary() ([]byte, error) {
+	return a.res.GetApp().MarshalJSON()
+}
+
+func (a *GetAppReply) Title() string {
+	return "App"
+}
+
+func (a *GetAppReply) Headers() []string {
+	return []string{"id", "name", "domains", "updated_at"}
+}
+
+func (a *GetAppReply) Fields() []map[string]string {
+	res := []map[string]string{}
+	item := a.res.GetApp()
+	fields := map[string]string{
+		"id":         renderer.FormatID(item.GetId(), a.full),
+		"name":       item.GetName(),
+		"domains":    formatDomains(item.GetDomains()),
+		"updated_at": renderer.FormatTime(item.GetUpdatedAt()),
+	}
+	res = append(res, fields)
+	return res
+}
---
+++ pkg/koyeb/renderer/item_renderer.go
@@ -0,0 +1,61 @@
+package renderer
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/ghodss/yaml"
+	"github.com/olekukonko/tablewriter"
+)
+
+func ItemRenderer(format string, item ApiResources) error {
+
+	switch format {
+	case "", "table":
+		var table *tablewriter.Table
+
+		table = tablewriter.NewWriter(os.Stdout)
+		table.SetAutoWrapText(false)
+		table.SetAutoFormatHeaders(true)
+		table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+		table.SetAlignment(tablewriter.ALIGN_LEFT)
+		table.SetCenterSeparator("")
+		table.SetColumnSeparator("")
+		table.SetRowSeparator("")
+		table.SetHeaderLine(false)
+		table.SetBorder(false)
+		table.SetTablePadding("\t")
+		table.SetNoWhiteSpace(true)
+		table.SetHeader(item.Headers())
+		fields := [][]string{}
+		for _, field := range item.Fields() {
+			current := []string{}
+			for _, h := range item.Headers() {
+				current = append(current, field[h])
+			}
+			fields = append(fields, current)
+		}
+		table.AppendBulk(fields)
+		table.Render()
+	case "yaml":
+		buf, err := item.MarshalBinary()
+		if err != nil {
+			return err
+		}
+		y, err := yaml.JSONToYAML(buf)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s", string(y))
+	case "json":
+		buf, err := item.MarshalBinary()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(buf))
+	default:
+		return errors.New("Invalid format")
+	}
+	return nil
+}
