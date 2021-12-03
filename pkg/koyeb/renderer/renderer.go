ce93cb47eb06f0aa25637eb945d4e22f589bfdc5
Bastien Chatelard <chatel.bast@gmail.com>
2021-12-03 23:27:36 +0100

    Add simple multi renderer

--- pkg/koyeb/renderer/renderer.go
+++ pkg/koyeb/renderer/renderer.go
@@ -1,5 +1,7 @@
 package renderer
 
+import "fmt"
+
 type WithTitle interface {
 	Title() string
 }
@@ -8,4 +10,17 @@ type ApiResources interface {
 	Headers() []string
 	Fields() []map[string]string
 	MarshalBinary() ([]byte, error)
+}
+
+func MultiRenderer(funcs ...func() error) error {
+	for i, f := range funcs {
+		if i > 0 && i <= len(funcs) {
+			fmt.Println("")
+		}
+		err := f()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
