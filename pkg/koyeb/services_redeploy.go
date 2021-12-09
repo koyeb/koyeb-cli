c400d0c5444877e92789150e7b4de5d2bcb40d7a
Bastien Chatelard <chatel.bast@gmail.com>
2021-12-09 11:08:28 +0100

    Fix redeploy

--- pkg/koyeb/services_redeploy.go
+++ pkg/koyeb/services_redeploy.go
@@ -1,12 +1,14 @@
 package koyeb
 
 import (
+	"github.com/koyeb/koyeb-api-client-go/api/v1/koyeb"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 func (h *ServiceHandler) ReDeploy(cmd *cobra.Command, args []string) error {
-	_, _, err := h.client.ServicesApi.ReDeploy(h.ctxWithAuth, h.ResolveServiceShortID(args[0])).Execute()
+	redeployBody := *koyeb.NewRedeployRequestInfoWithDefaults()
+	_, _, err := h.client.ServicesApi.ReDeploy(h.ctxWithAuth, h.ResolveServiceShortID(args[0])).Body(redeployBody).Execute()
 	if err != nil {
 		fatalApiError(err)
 	}
