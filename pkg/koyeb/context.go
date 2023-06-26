f8e42fb25c4060cfa743c88a03f36fb918dc2f9a
Julien Castets <castets.j@gmail.com>
2023-06-26 16:36:37 +0200

    CLIContext: stop using strings to identify variables in context
    With context.WithValue(ctx, "<name>", <value>), golangci-lint raises
    SA1029:
    
    > The provided key must be comparable and should not be of type string
    > or any other built-in type to avoid collisions between packages using
    > context. Users of WithValue should define their own types for keys.

--- pkg/koyeb/context.go
+++ pkg/koyeb/context.go
@@ -9,14 +9,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ctxkey int
+
+const (
+	ctx_client ctxkey = iota
+	ctx_mapper
+	ctx_renderer
+)
+
 // SetupCLIContext is called by the root command to setup the context for all subcommands.
 func SetupCLIContext(cmd *cobra.Command) {
 	apiClient := getApiClient()
 	ctx := cmd.Context()
 	ctx = context.WithValue(ctx, koyeb.ContextAccessToken, token)
-	ctx = context.WithValue(ctx, "client", apiClient)
-	ctx = context.WithValue(ctx, "mapper", idmapper.NewMapper(ctx, apiClient))
-	ctx = context.WithValue(ctx, "renderer", renderer.NewRenderer(outputFormat))
+	ctx = context.WithValue(ctx, ctx_client, apiClient)
+	ctx = context.WithValue(ctx, ctx_mapper, idmapper.NewMapper(ctx, apiClient))
+	ctx = context.WithValue(ctx, ctx_renderer, renderer.NewRenderer(outputFormat))
 	cmd.SetContext(ctx)
 }
 
@@ -34,10 +42,10 @@ func WithCLIContext(fn func(ctx *CLIContext, cmd *cobra.Command, args []string) 
 		ctx := cmd.Context()
 		cliContext := CLIContext{
 			Context:  ctx,
-			Client:   ctx.Value("client").(*koyeb.APIClient),
-			Mapper:   ctx.Value("mapper").(*idmapper.Mapper),
+			Client:   ctx.Value(ctx_client).(*koyeb.APIClient),
+			Mapper:   ctx.Value(ctx_mapper).(*idmapper.Mapper),
 			Token:    ctx.Value(koyeb.ContextAccessToken).(string),
-			Renderer: ctx.Value("renderer").(renderer.Renderer),
+			Renderer: ctx.Value(ctx_renderer).(renderer.Renderer),
 		}
 		return fn(&cliContext, cmd, args)
 	}
