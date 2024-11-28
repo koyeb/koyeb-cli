90dffbf07f2e203432f3cbab05985824e5c491e9
Bastien Chatelard <chatel.bast@gmail.com>
2024-11-28 15:12:49 +0100

    Improve debug messages

--- pkg/koyeb/deploy_addons.go
+++ pkg/koyeb/deploy_addons.go
@@ -15,6 +15,7 @@ func NewAddonsHandler(addons []string) (*addonsHandler, error) {
 	handler := &addonsHandler{}
 
 	for _, addon := range addons {
+		log.Infof("Registering addon: %s", addon)
 		err := handler.RegisterAddon(addon)
 		if err != nil {
 			return nil, err
@@ -43,7 +44,7 @@ func (h *addonsHandler) RegisterAddon(name string) error {
 
 func (h *addonsHandler) PreDeploy(ctx *CLIContext, definition *koyeb.DeploymentDefinition) error {
 	for _, addon := range h.addons {
-		log.Debugf("Running pre-deploy for addon %s", addon)
+		log.Debugf("Running addon pre-deploy")
 		if err := addon.PreDeploy(ctx, definition); err != nil {
 			return err
 		}
@@ -53,7 +54,7 @@ func (h *addonsHandler) PreDeploy(ctx *CLIContext, definition *koyeb.DeploymentD
 
 func (h *addonsHandler) PostDeploy(ctx *CLIContext, definition *koyeb.DeploymentDefinition) error {
 	for _, addon := range h.addons {
-		log.Debugf("Running post-deploy for addon %s", addon)
+		log.Debugf("Running addon post-deploy")
 		if err := addon.PostDeploy(ctx, definition); err != nil {
 			return err
 		}
@@ -63,7 +64,7 @@ func (h *addonsHandler) PostDeploy(ctx *CLIContext, definition *koyeb.Deployment
 
 func (h *addonsHandler) Setup(ctx *CLIContext, dir string) error {
 	for _, addon := range h.addons {
-		log.Debugf("Running setup for addon in dir %s", dir)
+		log.Debugf("Running addon setup in dir %s", dir)
 		if err := addon.Setup(ctx, dir); err != nil {
 			return err
 		}
@@ -73,7 +74,7 @@ func (h *addonsHandler) Setup(ctx *CLIContext, dir string) error {
 
 func (h *addonsHandler) Cleanup(ctx *CLIContext) error {
 	for _, addon := range h.addons {
-		log.Debugf("Running cleanup for addon %s", addon)
+		log.Debugf("Running addon cleanup")
 		if err := addon.Cleanup(ctx); err != nil {
 			return err
 		}
